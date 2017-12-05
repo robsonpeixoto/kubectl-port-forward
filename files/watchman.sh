709882d71153c3b6aa2b234b9bf7e68af613c5e3
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2017-12-05 09:40:31 -0300

    Better log message

--- files/watchman.sh
+++ files/watchman.sh
@@ -4,16 +4,16 @@ echo 'WATCHMAN: waiting 60s before start'
 sleep 60s
 
 while true; do
-    POD=`cat /scripts/pids/POD`
+    POD="$(cat /scripts/pids/POD)"
     if [ ! -z "$POD" ]; then
         kubectl get pod "$POD" > /dev/null
         STATUS="$?"
         if [ "$STATUS" -ne "0" ]; then
-            echo 'Restarting the process'
+            echo 'WATCHMAN: Restarting the process'
             python /app/create-configs-file.py
-            supervisorctl  status | awk '$1 !~ /watchman/ {print $1}'  | xargs supervisorctl restart
+            supervisorctl status | awk '$1 !~ /watchman/ {print $1}' | xargs supervisorctl restart
         fi
     fi
-    echo 'Waiting for 60 seconds'
+    echo 'WATCHMAN: repeate the process in 60s'
     sleep 60
 done
