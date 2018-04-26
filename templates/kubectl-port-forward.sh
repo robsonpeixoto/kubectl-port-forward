877851f1b7c4a0590a5a6ee7a7fea43eb3a2132a
Ygor Mutti <ygormutti@gmail.com>
2018-04-26 12:11:36 -0300

    Set -e flag to clean logs

--- templates/kubectl-port-forward.sh
+++ templates/kubectl-port-forward.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 POD="$(kubectl get pod -l {{ filters }} -o jsonpath='{.items[0].metadata.name}')"
 echo "$POD" > /scripts/pids/POD
 exec kubectl port-forward "$POD" {% for port in ports %} {{port.free}}:{{port.pod}} {% endfor %}
